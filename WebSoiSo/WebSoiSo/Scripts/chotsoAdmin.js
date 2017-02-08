d0fad792ee2a447910f887c97a34081ade37fbd2
DangTriHai <huutho2009@gmail.com>
2017-02-07 21:31:28 -0800

    Cm
    Hai

---
+++ WebSoiSo/WebSoiSo/Scripts/chotsoAdmin.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('#btnSave').click(function () {
+        var NgayTao = $('#dtpNgayTao').val();
+        var strErr;
+        if(NgayXo == null)
+            strErr = '<p style="color:red">Hãy chọn ngày xổ</p>';
+        if(NgayTao == null)
+            strErr += '<p style="color:red">Hãy chọn ngày tạo</p>';
+        if (strErr == '') {
+            $('#frmCreate').submit();
+        }
+        else {
+            $('#lstError').html(strErr);
+            $('body,html').animate({ scrollTop: 0 }, 600);
+            return false;
+        }
+    })
+});
